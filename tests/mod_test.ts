f423425c24e092de6747f71458af02da90f475d4
Okiki <okikio.dev@gmail.com>
2024-03-03 04:07:04 +0000

    chore: add Array tests
    Signed-off-by: Okiki <okikio.dev@gmail.com>

--- tests/mod_test.ts
+++ tests/mod_test.ts
@@ -1,5 +1,15 @@
-import { assertEquals } from "https://deno.land/std@0.208.0/testing/asserts.ts";
-import { asCodePointsIterator, asCodePointsCallback } from "../mod.ts";
+import { assertEquals } from "https://deno.land/std/assert/assert_equals.ts";
+import { asCodePointsArray, asCodePointsIterator, asCodePointsCallback } from "../mod.ts";
+
+Deno.test("asCodePoints Array - converts utf-8 filled Uint8Array's into code points", async () => {
+  const iterable = (async function* () {
+    yield new Uint8Array([0x61, 0xC3, 0xA9, 0xE0, 0xA4, 0xB9, 0xF0, 0x9F, 0x98, 0x82]);
+  })();
+
+  const result: number[] = await asCodePointsArray(iterable);
+
+  assertEquals(result, [0x61, 0xE9, 0x939, 0x1F602]);
+});
 
 Deno.test("asCodePoints Iterator - converts utf-8 filled Uint8Array's into code points", async () => {
   const iterable = (async function* () {
@@ -13,7 +23,6 @@ Deno.test("asCodePoints Iterator - converts utf-8 filled Uint8Array's into code 
 
   assertEquals(result, [0x61, 0xE9, 0x939, 0x1F602]);
 });
-
 
 Deno.test("asCodePoints Callback - converts utf-8 filled Uint8Array's into code points", async () => {
   const iterable = (async function* () {
